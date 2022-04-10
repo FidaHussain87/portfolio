e69c1465e46d47a1c9a551127c952d3c40e48f55
Fida Hussain <fidahussain.shahani@gmail.com>
2022-04-10 15:50:36 +0500

    service component completed

--- frontend/src/components/Services/Services.jsx
+++ frontend/src/components/Services/Services.jsx
@@ -14,20 +14,40 @@ const Services = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
           inventore quaerat alias nulla ipsa dicta facilis voluptates?
         </span>
-        <button className="button services-button">Download CV</button>
+        <a
+          href="https://drive.google.com/file/d/1YM9FxJvxPciV1xVxVoVtIb17mDwu08J1/view?usp=sharing"
+          download
+        >
+          <button className="button services-button">Download CV</button>
+        </a>
         <div
           className="blur service-blur-1"
           style={{ background: '#abf1ff94' }}
         ></div>
       </div>
       <div className="right">
-        <div>
+        <div style={{ left: '14rem' }}>
           <Card
             emoji={HeartEmoji}
             heading={'Design'}
-            detail={'Figma,Sketch,Photoshop AdobeXd,AdobeXd,'}
+            detail={'Figma, Sketch, Photoshop, AdobeXd, AdobeXd,'}
           />
         </div>
+        <div style={{ left: '-4rem', top: '12rem' }}>
+          <Card
+            emoji={Glasses}
+            heading={'Developer'}
+            detail={'HTML, CSS, JS, React, Nodejs,'}
+          />
+        </div>
+        <div style={{ left: '12rem', top: '19rem' }}>
+          <Card
+            emoji={Humble}
+            heading={'UI/UX'}
+            detail={'Fin-Tech, Ed-Tech, E-Commerce'}
+          />
+        </div>
+        <div className="blur service-blur-2" style={{background:"var(--purple)"}}></div>
       </div>
     </div>
   )
